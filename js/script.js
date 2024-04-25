0f3ce7b144210c456308ef46aba88b55f752517c
chiscojfr <jfrodriguezzayas@gmail.com>
2024-04-25 13:04:23 -0400

    v0.0.4

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
     function contactForm() {
         $('#contactform').submit(function () {
             var action = 'https://formspree.io/f/xnqelawk';
+            // var action = '';
             $("#message-info").slideUp(250, function () {
                 $('#message-info').hide();
                 $('#submit')
@@ -36,6 +37,46 @@
     }
 
     function messageSentAlert(){
+            var chapterPage = 'index.html';
+            var selectedChapter =  $('#chapter').val();
+            
+            switch(selectedChapter){
+                case 'Chapter I: Introduction':
+                    chapterPage = 'index.html';
+                    break;
+                case 'Chapter II: Injection Molding Process Parameters':
+                    chapterPage = 'chapter_2.html';
+                    break;
+                case 'Chapter III: Process Graphs':
+                    chapterPage = 'chapter_3.html';
+                    break;
+                case 'Chapter IV: Plastic Morphology':
+                    chapterPage = 'chapter_4.html';
+                    break;
+                case 'Chapter V: Auxiliary Equipment':
+                    chapterPage = 'chapter_5.html';
+                    break;
+                case 'Chapter VI: Molding from the Desk':
+                    chapterPage = 'chapter_6.html';
+                    break;
+                case 'Chapter VII: Machine Rheology':
+                    chapterPage = 'chapter_7.html';
+                    break;
+                case 'Chapter VIII: Determining Injection Speed':
+                    chapterPage = 'chapter_8.html';
+                    break;
+                case 'Chapter IX: Verifying Fill Balance':
+                    chapterPage = 'chapter_9.html';
+                    break;
+                case 'Chapter X: Determining Hold Stage Parameters':
+                    chapterPage = 'chapter_10.html';
+                    break;
+                case 'Chapter XI: Determining Cooling Stage Parameters':
+                    chapterPage = 'chapter_11.html';
+                    break;
+                default:
+                    chapterPage = 'index.html';
+            }
             $('#name').val("");
             $('#email').val("");
             $('#chapter').val("");
@@ -43,12 +84,16 @@
             $('#contactform .loader div').fadeOut('slow', function() {
                 $(this).remove();
             });
-            $('#submit').after('<p class="sent-alert" >SENT!</p>');
-            Swal.fire(
-                'Good job!',
-                'You message was sent! We will contact you soon.',
-                'success'
-              )
+            Swal.fire({
+            title: "Thank you!",
+            text: "You message was sent! We will contact you soon.",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Go back to chapter"
+            }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = chapterPage;
+            }
+            });
             $('#contactform .sent-alert').fadeOut('slow', function() {
                 $(this).remove();
             });
